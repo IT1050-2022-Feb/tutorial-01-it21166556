2ff4751552617d3ac953be52245634188f78e9f4
I.U.N.Nadeeshan <89896805+it21166556@users.noreply.github.com>
2022-02-08 04:56:24 +0000

    I have finished Tute 01

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 int main() {
+
+  float mark1, mark2, total = 0, average = 0; //Declare the variables
+
+  printf("Enter mark 1 : ");
+  scanf("%f", &mark1); //Input mark 1
+
+  printf("Enter mark 2 : ");
+  scanf("%f", &mark2); // Input mark 2
+
+  total = mark1 + mark2; // Calculate total marks
+  average = total / 2; // Calculate average marks
+
+  printf("Average of the tow marks is : %.2f", average); // Display the average marks
   
   return 0;
 }
